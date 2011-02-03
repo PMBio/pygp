72aa6ba43679203cef11e6bda19d8bced8ee8a1d
maxz <maxz@245335d9-b105-4bc3-a01e-ed8db82ea879>
2011-02-03 15:03:10 +0000

    git-svn-id: https://svn.tuebingen.mpg.de/ibg/pygp@1676 245335d9-b105-4bc3-a01e-ed8db82ea879

--- pygp/setup.py
+++ pygp/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='pygp',
       #namespace_packages=['pygp'],
-      version = '0.1.0',
-      install_requires = ['numpy','scipy']
+      version = '0.1.0'#,
+      #install_requires = ['numpy','scipy']
       )
