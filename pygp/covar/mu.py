6490136b2811317111fdc25dcdce9742ca396388
Nicolò Fusi <nicolo.fusi@gmail.com>
2011-06-02 11:47:51 +0100

    made MuCF inherit from FixedCF

--- pygp/covar/mu.py
+++ pygp/covar/mu.py
@@ -1,38 +1,20 @@
 import scipy as SP
 from pygp.covar import CovarianceFunction
+from pygp.covar.fixed import FixedCF
 
 
 
-class MuCF(CovarianceFunction):
+class MuCF(FixedCF):
     """isotropic mean parameter which is integrated out
     """
 
-    def __init__(self,*args,**kw_args):
-        super(CovarianceFunction, self).__init__(**kw_args)
-        self.n_hyperparameters = 1
-
-        
-    def K(self,theta,x1,x2=None):
-        #get input data:
-        # 2. exponentiate params:
-        A  = SP.exp(2*theta[0])
-        n = x1.shape[0]
-        RV = A*SP.ones([n,n])
-        return RV
-
-    def Kgrad_theta(self,theta,x1,i):
-        RV = self.K(theta,x1)
-        #derivative w.r.t. to amplitude
-        RV*=2
-        return RV
-
-
-    def Kgrad_x(self,theta,x1,x2,d):
-        RV = SP.zeros([x1.shape[0],x2.shape[0]])
-        return RV
-
-    
-    def Kgrad_xdiag(self,theta,x1,d):
-        """derivative w.r.t diagonal of self covariance matrix"""
-        RV = SP.zeros([x1.shape[0]])
-        return RV
+    def __init__(self, N, **kw_args):
+	"""
+	Constructor for the MuCF covariance function. It inherits everything from
+	FixedCF, as it is a special case of FixedCF with a matrix of ones.
+	
+	Input:
+	N = number of samples
+	"""
+	
+	super(MuCF, self).__init__(SP.ones((N,N)), **kw_args)
